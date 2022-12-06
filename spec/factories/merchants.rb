d6930c381ad12009faa05fd0ef64730f342d845e
Lucas Colwell <colwellc37@gmail.com>
2022-12-05 22:02:45 -0700

    Initial Commit

---
+++ spec/factories/merchants.rb
@@ -0,0 +1,6 @@
+FactoryBot.define do
+  factory :merchant do
+    # binding.pry
+    # name { Merchant.first.name }
+  end
+end
---
+++ spec/requests/api/v1/customers_request_spec.rb
@@ -0,0 +1,13 @@
+require 'rails_helper'
+
+describe "Merchants API" do
+  it "sends a list of Merchants" do
+    create_list(:merchant, 3)
+
+    get '/api/v1/merchants'
+
+    expect(response).to be_successful
+    # require "pry"; binding.pry
+    merchants = JSON.parse(response.body)
+  end
+end
