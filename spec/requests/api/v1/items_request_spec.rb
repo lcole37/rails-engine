5dc5e741b88e739fd0a29c2df4561dd7dc3e6717
Lucas Colwell <colwellc37@gmail.com>
2022-12-07 15:02:52 -0700

    Test: update for serializer

--- spec/requests/api/v1/items_request_spec.rb
+++ spec/requests/api/v1/items_request_spec.rb
@@ -29,16 +29,55 @@ describe " Items API" do
       expect(item).to have_key(:merchant_id)
       expect(item[:merchant_id]).to be_an(Integer)
     end
-
   end
 
-  # it "can return a list of items" do
-  #   create_list(:item, 7)
-  #
-  #   get "/api/v1/items"
-  #
-  #   response_body = JSON.parse(response.body, symbolize_names: true)
-  #
-  #
-  # end
+  it "can return a individual items" do
+    id = create(:item).id
+
+    get "/api/v1/items/#{id}"
+
+    item = JSON.parse(response.body, symbolize_names: true)
+
+    expect(response).to be_successful
+    # require "pry"; binding.pry
+    expect(item).to have_key(:id)
+    expect(item[:id]).to be_an(Integer)
+    expect(item[:id]).to eq(id)
+
+    expect(item).to have_key(:description)
+    expect(item[:description]).to be_an(String)
+
+    expect(item).to have_key(:unit_price)
+    expect(item[:unit_price]).to be_an(Float)
+
+    expect(item).to have_key(:merchant_id)
+    expect(item[:merchant_id]).to be_an(Integer)
+  end
+
+  it 'can return items for given merchant' do
+    @merchant = create(:merchant)
+    items = create_list(:item, 3, merchant: @merchant)
+
+    # get "/api/v1/merchants/#{@merchant.id}/items"
+    get api_v1_merchant_items_path(@merchant.id)
+
+    expect(response).to be_successful
+
+    items_response = JSON.parse(response.body, symbolize_names: true)
+
+    # require "pry"; binding.pry
+
+    expect(items_response.count).to eq(3)
+
+    items_response.each do |item|
+      expect(item[:id]).to be_a(Integer)
+      expect(item[:name]).to be_a(String)
+      expect(item[:description]).to be_a(String)
+      expect(item[:merchant_id]).to be_a(Integer)
+    end
+  end
+
+  it "can create a new item" do
+    #items create
+  end
 end
