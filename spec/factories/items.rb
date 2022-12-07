92ee374afc9d03006604e7b08813ba3ab00cec75
Lucas Colwell <colwellc37@gmail.com>
2022-12-07 15:03:30 -0700

    Update faker call

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :item do
     name { Faker::Commerce.product_name }
     description { Faker::Lorem.paragraph}
-    unit_price { 2.5 }
+    unit_price { Faker::Number.decimal(l_digits: 2, r_digits: 2) }
     merchant { create(:merchant) }
   end
 end
