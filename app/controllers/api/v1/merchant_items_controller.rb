88b869a56a38970e6c73dd1f45463e9a5adc99d7
Lucas Colwell <colwellc37@gmail.com>
2022-12-08 15:51:06 -0700

    Add search controller

--- app/controllers/api/v1/merchant_items_controller.rb
+++ app/controllers/api/v1/merchant_items_controller.rb
@@ -1,7 +1,11 @@
 class Api::V1::MerchantItemsController < ApplicationController
   def index
-    merchant = Merchant.find(params[:merchant_id])
-    items = merchant.items
-    render json: ItemSerializer.new(items)
+    # if Merchant.exists?(params[:merchant_id])
+      merchant = Merchant.find(params[:merchant_id])
+      items = merchant.items
+      render json: ItemSerializer.new(items)
+    # else
+    #   render json: {error: "error; not found"}, status: 404
+    # end
   end
 end
